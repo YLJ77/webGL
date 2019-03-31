da762b3a8126c131fa35c11749cfb233a51f58fd
boom <linjieyang77@gmail.com>
2019-03-31 09:59:48 +0800

    【新增】 添加坐标转换和buffer的初始化和绑定功能

--- src/util/appFunc.js
+++ src/util/appFunc.js
@@ -22,3 +22,26 @@ export function createProgram({ ctx, vSource, fSource }) {
     ctx.useProgram(program);
     return program;
 }
+
+export function windowToCanvas({x, y, canvas }) {
+    let rect = canvas.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    return { x, y }
+}
+
+export function initVertexBuffers({ ctx, vertices, attrVar, program }) {
+    // Bind the buffer object to target
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, ctx.createBuffer());
+    // Write date into the buffer object
+    ctx.bufferData(ctx.ARRAY_BUFFER, vertices, ctx.STATIC_DRAW);
+
+    let a_Position = ctx.getAttribLocation(program, attrVar);
+    // Assign the buffer object to a_Position variable
+    ctx.vertexAttribPointer(a_Position, 2, ctx.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    ctx.enableVertexAttribArray(a_Position);
+
+    return vertices.length / 2;
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-    <canvas id="webgl" width="400" height="400">
+    <canvas id="webgl" width="400" height="400" @click="addPoint">
         Please use a browser that supports "canvas"
     </canvas>
 </template>
 
 
 <script>
-    import { createProgram } from "../util/appFunc";
+    import { createProgram, windowToCanvas, initVertexBuffers } from "../util/appFunc";
 
     export default {
         data() {
             return {
                 canvas: null,
                 ctx: null,
+                g_points: [],
+                program: null,
                 VSHADER_SOURCE:
                     'attribute vec4 a_Position;\n' +
                     'attribute float a_PointSize;\n' +
@@ -23,41 +25,63 @@
 
                 // Fragment shader program
                 FSHADER_SOURCE:
+                    'precision mediump float;\n' +
+                    'uniform vec4 u_FragColor;\n' +
                     'void main() {\n' +
-                    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+                    '  gl_FragColor = u_FragColor;\n' + // Set the point color
                     '}\n'
             }
         },
         methods: {
-            init() {
-                // Retrieve <canvas> element
-                this.canvas = document.getElementById('webgl');
-                // Get the rendering context for WebGL
-                this.ctx = this.canvas.getContext('webgl');
-            },
-            draw() {
-                let { ctx, VSHADER_SOURCE, FSHADER_SOURCE } = this;
-                let program = createProgram({ ctx, vSource: VSHADER_SOURCE, fSource: FSHADER_SOURCE });
-                // Get the storage location of attribute variable
-                let a_Position = ctx.getAttribLocation(program, 'a_Position');
-                let a_PointSize = ctx.getAttribLocation(program, 'a_PointSize');
-                // Pass vertex position to attribute variable
-                ctx.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-                ctx.vertexAttrib1f(a_PointSize, 20.0);
+            drawTriangle() {
+                let { ctx, program } = this;
+                let vertices = new Float32Array([ 0.0, 0.5,   -0.5, -0.5,   0.5, -0.5 ]);
+                // Write the positions of vertices to a vertex shader
+                let n = initVertexBuffers({ ctx, program, vertices, attrVar: 'a_Position' });
 
                 // Specify the color for clearing <canvas>
-                ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+                ctx.clearColor(0, 0, 0, 1);
 
                 // Clear <canvas>
                 ctx.clear(ctx.COLOR_BUFFER_BIT);
 
-                // Draw a point
-                ctx.drawArrays(ctx.POINTS, 0, 1);
-            }
+                // Draw three points
+                ctx.drawArrays(ctx.LINE_LOOP, 0, n);
+                ctx.disableVertexAttribArray(ctx.getAttribLocation(program, 'a_Position'));
+            },
+            init() {
+                let { VSHADER_SOURCE, FSHADER_SOURCE } = this;
+                // Retrieve <canvas> element
+                this.canvas = document.getElementById('webgl');
+                // Get the rendering context for WebGL
+                this.ctx = this.canvas.getContext('webgl');
+                let { ctx } = this;
+                this.program = createProgram({ ctx, vSource: VSHADER_SOURCE, fSource: FSHADER_SOURCE });
+/*                let a_PointSize = ctx.getAttribLocation(this.program, 'a_PointSize');
+                ctx.vertexAttrib1f(a_PointSize, 10.0);*/
+                ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+                ctx.clear(ctx.COLOR_BUFFER_BIT);
+            },
+            addPoint(e) {
+                let { canvas, g_points, ctx, program } = this;
+                let { clientX: x, clientY: y } = e;
+                let loc = windowToCanvas({ x, y, canvas });
+                let a_Position = ctx.getAttribLocation(program, 'a_Position');
+                loc.color = [Math.random(), Math.random(), Math.random(), 1.0],
+                g_points.push(loc);
+                ctx.clear(ctx.COLOR_BUFFER_BIT);
+                g_points.forEach(point => {
+                    let { x, y, color } = point;
+                    let u_FragColor = ctx.getUniformLocation(program, 'u_FragColor');
+                    ctx.vertexAttrib3f(a_Position, x, y, 0.0);
+                    ctx.uniform4fv(u_FragColor, color)
+                    ctx.drawArrays(ctx.POINTS, 0, 1);
+                });
+            },
         },
         mounted() {
             this.init();
-            this.draw();
+            this.drawTriangle();
         }
     }
 </script>
