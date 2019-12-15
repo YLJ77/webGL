39899b85663948960f93d115314c30fa47c6ef63
boom <linjieyang77@gmail.com>
2019-12-15 22:00:45 +0800

    [alter] Transformation Matrix:Moving, Rotating, and Scaling

--- src/views/multiPoints/index.vue
+++ src/views/multiPoints/index.vue
@@ -22,10 +22,36 @@
             main() {
                 const { gl } = this.$refs.canvasWrap;
                 const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+                const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+                const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+                // Rotation angle
+                const ANGLE = 30.0;
+                // Pass the data required to rotate the shape to the vertex shader
+                const radian = Math.PI * ANGLE / 180.0; // Convert to radians
+                const cosB = Math.cos(radian);
+                const sinB = Math.sin(radian);
+
+                const xformMatrix = new Float32Array([
+                    cosB, sinB, 0.0, 0.0,
+                    -sinB, 1.5*cosB, 0.0, 0.0,
+                    0.0, 0.0, 1.0, 0.0,
+                    0.5, 0.5, 0.0, 1.0
+                ]);
+
+                // Pass the rotation matrix to the vertex shader
+                const u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+                gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+/*                const u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+                const u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+                gl.uniform1f(u_CosB, cosB);
+                gl.uniform1f(u_SinB, sinB);*/
+                gl.uniform4fv(u_FragColor, new Float32Array([1.0,1,0,1.0]));
+                gl.uniform4f(u_Translation, 0.5, 0.5, 0, 0.0);
                 gl.vertexAttrib1f(a_PointSize, 10);
                 initVertexBuffers({
                    gl,
                    vertices: new Float32Array([0.0, 0.5,   -0.5, -0.5,   0.5, -0.5]),
+                   //  vertices: new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5]),
                     program: gl.program,
                     verticesInfo: [
                         {
@@ -41,7 +67,7 @@
                 // Clear <canvas>
                 gl.clear(gl.COLOR_BUFFER_BIT);
                 // Draw three points
-                gl.drawArrays(gl.POINTS, 0, 3);
+                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
             }
         }
     }
--- src/views/point/shaders/point.vert
+++ src/views/point/shaders/point.vert
@@ -1,6 +1,14 @@
 attribute vec4 a_Position;
 attribute float a_PointSize;
+uniform vec4 u_Translation;
+uniform float u_CosB, u_SinB;
+uniform mat4 u_xformMatrix;
 void main() {
-    gl_Position = a_Position;
+//    gl_Position = a_Position + u_Translation;
+/*    gl_Position.x = a_Position.x * u_CosB - a_Position.y *u_SinB;
+    gl_Position.y = a_Position.x * u_SinB + a_Position.y * u_CosB;
+    gl_Position.z = a_Position.z;
+    gl_Position.w = 1.0;*/
+    gl_Position = u_xformMatrix * a_Position;
     gl_PointSize = a_PointSize;
 }
